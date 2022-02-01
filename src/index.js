e9c97d7ca765e247d964862b4b5a50dd675027bd
Andrey Barsukov <andrey.bv.gm@gmail.com>
2022-02-01 12:05:32 +0500

    Реализация собственного Middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { asyncIncrement, decrement, increment } from './redux/action';
 import { rootReducer } from './redux/rootReducer';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 // import { DECREMENT, INCREMENT } from './redux/types';
 import './styles.css';
 
@@ -12,7 +13,24 @@ const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 
-const store = createStore(rootReducer, 0, applyMiddleware(thunk));
+// ! Реализация middleware
+function logger(state) {
+	return function (next) {
+		return function (action) {
+			console.log('Prev state', state.getState());
+			const newState = next(action);
+			// console.log('Next state', newState.getState());
+			return newState;
+		};
+	};
+}
+
+const store = createStore(
+	rootReducer,
+	0,
+	// applyMiddleware(thunk)
+	applyMiddleware(thunk, logger)
+);
 
 // window.store = store;
 
